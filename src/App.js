ebf013d004416770503e943e6964ab03c13445fb
unknown <dagimt369@gmail.com>
2025-07-19 16:08:37 +0200

    initial setup (config webkit, babel, react)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+      <>
+        <h1>Addis Song Manager</h1>
+        <p>Welcome to the Addis Song Manager application!</p>
+      </>
+  )
+}
+
+export default App
